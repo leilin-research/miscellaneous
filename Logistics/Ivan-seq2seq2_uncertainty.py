9ae571a2ba9f991ac1686492ff0eca2fd9d3ac5e
transpaper <llin22@buffalo.edu>
2019-08-10 14:15:57 -0400

    generate results

--- Logistics/Ivan-seq2seq2_uncertainty.py
+++ Logistics/Ivan-seq2seq2_uncertainty.py
@@ -16,9 +16,15 @@ from keras.layers import Flatten
 from keras.layers import LSTM
 from keras.layers import RepeatVector
 from keras.layers import TimeDistributed
-from keras.layers import Dropout
+from keras.layers import Dropout, Activation
+from keras import backend as K
+from keras.layers.core import Lambda
 from sklearn.preprocessing import MinMaxScaler
+
 import csv
+
+def PermaDropout(rate):
+    return Lambda(lambda x: K.dropout(x, level=rate))
 
 # read training and test dataset
 #dataset = pd.read_csv('household_power_consumption_days.csv', header=0, infer_datetime_format=True, parse_dates=['datetime'], index_col=['datetime'])
@@ -85,7 +91,7 @@ for _ in range(len(data)):
             #X.append(x_input)
             ####### for multivariate input  ######
             X.append(data[in_start:in_end,:])
-            y.append(data[in_end:out_end,3])
+            y.append(data[in_end:out_end,3]) # 3 is the total demand
             # move along one time step
             in_start += 1
     
@@ -114,11 +120,12 @@ model.add(RepeatVector(n_outputs))
 #model.add(Dense(100, activation='relu'))
 #model.add(Dense(n_outputs))
 model.add(LSTM(360, activation='relu', return_sequences=True)) #decoder #200编码空间的潜在维数
-model.add(Dropout(0.2))
+model.add(PermaDropout(0.2))
 model.add(TimeDistributed(Dense(36, activation='linear'))) #for each time step build a hidden layer, the output is 36
-model.add(Dropout(0.2))
+#model.add(PermaDropout(0.2))
 model.add(TimeDistributed(Dense(1))) # for each time step build a hidden layer, the output is 1 now
-model.add(Dropout(0.2))
+#model.add(PermaDropout(0.2))
+model.add(Activation('sigmoid'))
 model.compile(loss='mse', optimizer='nadam') #loss= mse, optimizer=adam, 
 #'SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam'
 #mse，mae，mape，msle，squared_hinge，hinge，binary_crossentropy， categorical_crossentropy
@@ -126,63 +133,90 @@ model.compile(loss='mse', optimizer='nadam') #loss= mse, optimizer=adam,
 model.fit(train_x, train_y, epochs=epochs, batch_size=batch_size, verbose=verbose)
 
  # make a forecast
- 
+all_preds = []
+RMSE = []
+total = 1000
+
+for i in range(total):
  # history is a list of weekly data
-history = [x for x in train]  # list
- # walk-forward validation over each week
-predictions = list()
-for i in range(len(test)):
-		# predict the week
-		#yhat_sequence = forecast(model, history, n_input)
-        # flatten data
-        data = np.array(history)
-        data = data.reshape((data.shape[0]*data.shape[1], data.shape[2]))
-        # retrieve last observations for input data
-        #input_x = data[-n_input:, 3] # univariate input
-        input_x = data[-n_input:, :] # multivariate input
-        # reshape into [1, n_input, 1],for univariate input
-        #input_x = input_x.reshape((1, len(input_x), 1))  #samples*timesteps*features
-        # reshape into [1, n_input, n_features]
-        input_x = input_x.reshape((1, input_x.shape[0], input_x.shape[1]))  #samples*timesteps*features
-        # forecast the next week
-        yhat = model.predict(input_x, verbose=0)   # samples*timesteps
-        # we only want the vector forecast
-        yhat_seq = yhat[0]
-        # store the predictions
-        predictions.append(yhat_seq)
-        # get real observation and add to history for predicting the next week
-        history.append(test[i, :])
-    
-    
-    
-# evaluate predictions days for each week
-predictions = np.array(predictions)  # samples(test weeks)*timesteps*n_features
-#inverse scale all predictions and obs
+	history = [x for x in train]  # list
+	 # walk-forward validation over each week
+	predictions = list()
+	for i in range(len(test)):
+			# predict the week
+			#yhat_sequence = forecast(model, history, n_input)
+		# flatten data
+		data = np.array(history)
+		data = data.reshape((data.shape[0]*data.shape[1], data.shape[2]))
+		# retrieve last observations for input data
+		#input_x = data[-n_input:, 3] # univariate input
+		input_x = data[-n_input:, :] # multivariate input
+		# reshape into [1, n_input, 1],for univariate input
+		#input_x = input_x.reshape((1, len(input_x), 1))  #samples*timesteps*features
+		# reshape into [1, n_input, n_features]
+		input_x = input_x.reshape((1, input_x.shape[0], input_x.shape[1]))  #samples*timesteps*features
+		# forecast the next week
+		yhat = model.predict(input_x, verbose=0)   # samples*timesteps
+		# we only want the vector forecast
+		yhat_seq = yhat[0]
+		# store the predictions
+		predictions.append(yhat_seq)
+		# get real observation and add to history for predicting the next week
+		history.append(test[i, :])
+	    
+	    
+	    
+	# evaluate predictions days for each week
+	predictions = np.array(predictions)  # samples(test weeks)*timesteps*n_features
+	
+	score, scores = evaluate_forecasts(test[:, :, 3], predictions) #3 预测total
+
+	RMSE.append(score)
+
+	#irenverse scale all predictions and obs
+	pred_seq = predictions.reshape(predictions.shape[0]*predictions.shape[1],predictions.shape[2])
+	
+	all_preds.append(pred_seq)
+
+	#for i in range(len(test)):
+	
+	#print (pred_seq.shape)
+
+#print (len(all_preds))
+
+all_preds = np.array(all_preds)
+aver = np.mean(RMSE)
+print (aver)
+#print (np.mean(all_preds, axis = 0))
+#print (np.std(all_preds, axis = 0))
 
 #test = scaler.inverse_transform(test)
 #predictions = scaler.inverse_transform(predictions)
+pre_mean = np.mean(all_preds, axis = 0)
+pre_std = np.std(all_preds, axis = 0)
 
-
-#for i in range(len(test)):
-score, scores = evaluate_forecasts(test[:, :, 3], predictions) #3 预测total
+np.savetxt("mean"+str(aver)+".csv", pre_mean, delimiter=",")
+np.savetxt("std"+str(aver)+".csv", pre_std, delimiter=",")
  
  # summarize scores
-summarize_scores('lstm', score, scores)
+#summarize_scores('lstm', score, scores)
 # plot scores
-x = np.arange(7)
+#x = np.arange(7)
 #days = ['sun', 'mon', 'tue', 'wed', 'thr', 'fri', 'sat']
-plt.plot(x, scores, marker='o')
-plt.ylabel('mape')
-plt.show()
+#plt.plot(x, scores, marker='o')
+#plt.ylabel('mape')
+#plt.show()
  
  
 
-pred_seq = predictions.reshape(predictions.shape[0]*predictions.shape[1],predictions.shape[2])
+
 obs_seq = test[:,:,3].reshape(test.shape[0]*test.shape[1],1)#3 预测total
 
 x = np.arange(len(obs_seq))
-plt.plot(x, pred_seq)
-plt.plot(x, obs_seq)
+plt.plot(x, pred_seq, label = 'one prediction')
+plt.plot(x, obs_seq, label = 'real obs')
+plt.plot(x, pre_mean, label = 'mean')
+plt.legend()
 plt.show()
 
 
