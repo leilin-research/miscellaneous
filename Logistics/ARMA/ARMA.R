18ffa40044dd214502cbba2224860f5639efdab3
transpaper <llin22@buffalo.edu>
2019-08-11 09:45:47 -0400

    adding R

---
+++ Logistics/ARMA/ARMA.R
@@ -0,0 +1,19 @@
+data1<-read.csv('Total_Demand.csv');
+library(forecast);
+train<-data1[1:630,5];
+test<-data1[601:900,1];
+
+
+# off line training
+sensor<-ts(train,frequency=15);
+fit <- auto.arima(sensor,approximation=FALSE,trace=FALSE);
+fcast <- forecast(fit,h=300,level=c(80,95));# typeof(fcast)
+res <- data.frame(fcast[[4]],fcast[[5]],fcast[[6]]);#transfer time series back to data frame
+write.csv(res, "ARMA_res.csv") 
+# the five columns are point predictions, lower bound 80, lower bound 95, upper bound 80, upper bound 95
+
+
+
+
+
+
