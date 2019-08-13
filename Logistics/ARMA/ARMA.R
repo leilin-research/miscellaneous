e1b4a7d5493cae20b4a52d95ed0d23820cb48f99
transpaper <llin22@buffalo.edu>
2019-08-12 21:04:10 -0600

    pso-elm

--- Logistics/ARMA/ARMA.R
+++ Logistics/ARMA/ARMA.R
@@ -1,13 +1,13 @@
 data1<-read.csv('Total_Demand.csv');
 library(forecast);
 train<-data1[1:630,5];
-test<-data1[601:900,1];
+test<-data1[632:701,5];
 
 
 # off line training
-sensor<-ts(train,frequency=15);
+sensor<-ts(train,frequency=7);
 fit <- auto.arima(sensor,approximation=FALSE,trace=FALSE);
-fcast <- forecast(fit,h=300,level=c(80,95));# typeof(fcast)
+fcast <- forecast(fit,h=70,level=c(80,95));# typeof(fcast)
 res <- data.frame(fcast[[4]],fcast[[5]],fcast[[6]]);#transfer time series back to data frame
 write.csv(res, "ARMA_res.csv") 
 # the five columns are point predictions, lower bound 80, lower bound 95, upper bound 80, upper bound 95
